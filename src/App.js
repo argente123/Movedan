7eb3ddad1e6f4ffacf05cf4cf10f7c8a930fff24
José Daniel Silva do Carmo <dan321daniel@gmail.com>
2021-03-03 11:24:22 -0300

    main

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { ExperienceBar } from './components/ExperienceBar';
+import './Styles/global.css';
+
+function App() {
+  return (
+    <div className="conteiner">
+      <ExperienceBar />
+    </div>
+  );
+}
+
+export default App;
